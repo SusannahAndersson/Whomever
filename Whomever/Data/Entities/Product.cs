8c72cbb13f0f2536f2ce93b80d81f2b6f515998d
SusannahAndersson <susannah.alx@gmail.com>
2022-05-20 15:34:07 +0200

    added product entity

---
+++ Whomever/Data/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Whomever.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public string ProductId { get; set; }
+    }
+}
