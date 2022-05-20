b19a5882a65807c3a9bf9072b0b85b914d22bdd6
SusannahAndersson <susannah.alx@gmail.com>
2022-05-20 11:51:33 +0200

    update site.js

--- Whomever/wwwroot/js/site.js
+++ Whomever/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    var x = 0;
+    var s = "";
+
+    console.log("Testing");
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#addtocartButton");
+    button.on("click", function () {
+        console.log("Added to cart");
+    });
+
+    var productInfo = $(".products-prop li");
+    productInfo.on("click", function () {
+        console.log($(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(100);
+    });
+});
