f239282e6748e1e437d01ab1302c7ecb55d6dd11
SusannahAndersson <susannah.alx@gmail.com>
2022-05-20 18:21:29 +0200

    added webshop to homecontroller

--- Whomever/Controllers/HomeController.cs
+++ Whomever/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Whomever.Data;
 using Whomever.Models;
 
 namespace Whomever.Controllers
@@ -6,10 +7,12 @@ namespace Whomever.Controllers
     public class HomeController : Controller
     {
         private readonly Areas.ContactUsService.IContactUsService _contactUsService;
+        private readonly ApplicationDbContext applicationDbContext;
 
-        public HomeController(Areas.ContactUsService.IContactUsService contactUsService)
+        public HomeController(Areas.ContactUsService.IContactUsService contactUsService, ApplicationDbContext applicationDbContext)
         {
             _contactUsService = contactUsService;
+            this.applicationDbContext = applicationDbContext;
         }
 
         //IActionResult to map logic to view
@@ -42,11 +45,20 @@ namespace Whomever.Controllers
         {
             return View();
         }
-    }
 
-    //    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    //    public IActionResult Error()
-    //    {
-    //        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    //    }
+        public IActionResult WebShop()
+        {
+            //passing data from db to view
+            var results = applicationDbContext.Products
+                .OrderBy(p => p.Category)
+                .ToList();
+            return View(results.ToList());
+        }
+
+        //    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //    public IActionResult Error()
+        //    {
+        //        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //    }
+    }
 }
