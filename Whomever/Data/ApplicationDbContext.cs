22602997f9d4b681afef981d41e120b1ed3184cf
SusannahAndersson <susannah.alx@gmail.com>
2022-05-20 18:23:09 +0200

    added initial migration to database

--- Whomever/Data/ApplicationDbContext.cs
+++ Whomever/Data/ApplicationDbContext.cs
@@ -15,5 +15,17 @@ namespace Whomever.Data
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+               : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+
+        //    optionsBuilder.UseSqlServer();
+        //}
     }
 }
