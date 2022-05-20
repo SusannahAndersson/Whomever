70d318714503b2d19fd8915abf65d94fcf8de26b
SusannahAndersson <susannah.alx@gmail.com>
2022-05-20 18:19:45 +0200

    db con

--- Whomever/Program.cs
+++ Whomever/Program.cs
@@ -1,19 +1,25 @@
+using Microsoft.EntityFrameworkCore;
 using Whomever.Areas.ContactUsService;
 using Whomever.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(connectionString));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
 //    .AddEntityFrameworkStores<ApplicationDbContext>();
 //builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>();
+//database
+//builder.Services.AddDbContext<ApplicationDbContext>(cfg =>
+//{
+//    cfg.UseSqlServer();
+//}
+//);
 
 builder.Services.AddTransient<IContactUsService, NullContactUsService>();
 
