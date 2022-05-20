38aeacf4cb1f080b8255b72e1cb277446f4aae4d
SusannahAndersson <susannah.alx@gmail.com>
2022-05-20 15:33:41 +0200

    added order entity

---
+++ Whomever/Data/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Whomever.Data.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderNumber { get; set; }
+
+        //relate order entity to orderitem entity w one-to-many relationship
+        public ICollection<OrderItem> Items { get; set; }
+    }
+}
