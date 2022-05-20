6bba81e181e93b08b5d919ce504a72fad2fd550b
SusannahAndersson <susannah.alx@gmail.com>
2022-05-20 16:13:09 +0200

    added dbset for product and order

--- Whomever/Data/ApplicationDbContext.cs
+++ Whomever/Data/ApplicationDbContext.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Whomever.Data.Entities;
 
 namespace Whomever.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    //public class ApplicationDbContext : IdentityDbContext
+    //{
+    //    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    //        : base(options)
+    //    {
+    //    }
+    //}
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
