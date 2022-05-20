82f4337d721fa44cf4dc57471d7a0d90422d893b
SusannahAndersson <susannah.alx@gmail.com>
2022-05-20 09:33:22 +0200

    added conctactviewmodel

---
+++ Whomever/Models/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Whomever.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        [MaxLength(200, ErrorMessage = "Too long")]
+        public string Message { get; set; }
+    }
+}
