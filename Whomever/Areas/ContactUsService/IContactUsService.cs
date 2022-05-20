6a8952da4bd85b6f6b7959e6aec74bf3f3cc9588
SusannahAndersson <susannah.alx@gmail.com>
2022-05-20 09:31:16 +0200

    added contactusservice

---
+++ Whomever/Areas/ContactUsService/IContactUsService.cs
@@ -0,0 +1,9 @@
+﻿//Only used to log ui to console
+//Interface: in case to actually implement extended mail service usage (with auto ext)
+namespace Whomever.Areas.ContactUsService
+{
+    public interface IContactUsService
+    {
+        void SendMessage(string to, string subject, string body);
+    }
+}
---
+++ Whomever/Areas/ContactUsService/NullContactUsService.cs
@@ -0,0 +1,20 @@
+﻿//Only used to log ui to console
+//Interface: in case to actually implement extended mail service usage (with auto ext)
+namespace Whomever.Areas.ContactUsService
+{
+    public class NullContactUsService : IContactUsService
+    {
+        private readonly ILogger<NullContactUsService> _logger;
+
+        public NullContactUsService(ILogger<NullContactUsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void SendMessage(string to, string subject, string body)
+        {
+            //log msg
+            _logger.LogInformation($"To: {to} Subject: {subject} Body: {body}");
+        }
+    }
+}
