3e5fc7166c3a758ff637cf518fefc3f809caf34a
SusannahAndersson <susannah.alx@gmail.com>
2022-05-24 14:48:50 +0200

    added apprepo

---
+++ Whomever/Data/ApplicationRepository.cs
@@ -0,0 +1,36 @@
+﻿using Whomever.Data.Entities;
+
+namespace Whomever.Data
+{
+    public class ApplicationRepository : IApplicationRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ApplicationRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        //default
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _applicationDbContext.Products
+                .OrderBy(p => p.Title)
+                .ToList();
+        }
+
+        //category
+        public IEnumerable<Product> GetProductsByCategory(string Category)
+        {
+            return _applicationDbContext.Products
+                .Where(p => p.Category == Category)
+                //.OrderByDescending(p => p.Title)
+                .ToList();
+        }
+
+        public bool SaveAll()
+        {
+            return _applicationDbContext.SaveChanges() > 0;
+        }
+    }
+}
---
+++ Whomever/Data/IApplicationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Whomever.Data.Entities;
+
+namespace Whomever.Data
+{
+    //for testing mock data
+    public interface IApplicationRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+
+        IEnumerable<Product> GetProductsByCategory(string Category);
+
+        bool SaveAll();
+    }
+}
