7cdc156a343c0414589239cf93c684b6cb572488
SusannahAndersson <susannah.alx@gmail.com>
2022-05-24 18:23:42 +0200

    added productscontroller w httpget

---
+++ Whomever/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Whomever.Data;
+using Whomever.Data.Entities;
+
+namespace Whomever.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductsController : Controller//Base
+    {
+        private readonly IApplicationRepository _applicationRepository;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IApplicationRepository applicationRepository, ILogger<ProductsController> logger)
+        {
+            _applicationRepository = applicationRepository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult Get()
+        //public actionResult<IEnumerable<Product>>
+        {
+            try
+            {//200
+                return Ok(_applicationRepository.GetAllProducts());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get products: {ex}");
+                //400
+                //return BadRequest(ex.Message);
+                return BadRequest("Failed to get products");
+            }
+        }
+    }
+}
