0c2fdf49149898151111c5665494dea168d7d9b0
SusannahAndersson <susannah.alx@gmail.com>
2022-05-24 14:49:28 +0200

    added appseeder

---
+++ Whomever/Data/ApplicationSeeder.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using Whomever.Data.Entities;
+
+namespace Whomever.Data
+{
+    public class ApplicationSeeder
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ApplicationSeeder(ApplicationDbContext applicationDbContext, IWebHostEnvironment webHostEnvironment)
+        {
+            _applicationDbContext = applicationDbContext;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public void Seed()
+        {
+            _applicationDbContext.Database.EnsureCreated();
+
+            if (!_applicationDbContext.Products.Any())
+            {
+                // Need to create the Sample Data
+                var file = Path.Combine(_webHostEnvironment.ContentRootPath, "Data/whproduct.json");
+                var json = File.ReadAllText(file);
+                var products = JsonSerializer.Deserialize<IEnumerable<Product>>(json);
+
+                _applicationDbContext.Products.AddRange(products);
+
+                var order = new Order()
+                {
+                    OrderDate = DateTime.Today,
+                    OrderNumber = "5000",
+
+                    Items = new List<OrderItem>()
+                                {
+                                    new OrderItem()
+                                    {
+                                        Product = products.First(),
+                                        Quantity = 1,
+                                        UnitPrice = products.First().Price
+                                    }
+                                }
+                };
+                _applicationDbContext.Orders.Add(order);
+                _applicationDbContext.SaveChanges();
+            }
+        }
+    }
+}
