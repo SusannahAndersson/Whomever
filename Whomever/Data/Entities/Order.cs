3c1a48cab43b7154fec58e1a2d31dc6e060759c1
SusannahAndersson <susannah.alx@gmail.com>
2022-05-25 09:17:45 +0200

    (wip temp) added allow nullable ordernumber, items for new order

--- Whomever/Data/Entities/Order.cs
+++ Whomever/Data/Entities/Order.cs
@@ -4,9 +4,10 @@
     {
         public int Id { get; set; }
         public DateTime OrderDate { get; set; }
-        public string OrderNumber { get; set; }
+        public string? OrderNumber { get; set; }
 
         //relate order entity to orderitem entity w one-to-many relationship
-        public ICollection<OrderItem> Items { get; set; }
+
+        public ICollection<OrderItem>? Items { get; set; }
     }
 }
