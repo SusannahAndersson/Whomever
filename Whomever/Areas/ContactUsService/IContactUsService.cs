3968805ba28ab32b0c37e84c9b57fd26d338e7ee
SusannahAndersson <susannah.alx@gmail.com>
2022-05-27 10:52:41 +0200

    removed areas and moved contactusservice to data/contactusfolder

--- Whomever/Areas/ContactUsService/IContactUsService.cs
+++
@@ -1,9 +0,0 @@
-﻿//Only used to log ui to console
-//Interface: in case to actually implement extended mail service usage (with auto ext)
-namespace Whomever.Areas.ContactUsService
-{
-    public interface IContactUsService
-    {
-        void SendMessage(string to, string subject, string body);
-    }
-}
