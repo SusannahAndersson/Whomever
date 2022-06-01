bfe64324ba891ea6c4673aaccc03fb6b58ba33d7
SusannahAndersson <susannah.alx@gmail.com>
2022-06-01 22:45:39 +0200

    added username and password prop for applicationuser

--- Whomever/Data/Entities/ApplicationUser.cs
+++ Whomever/Data/Entities/ApplicationUser.cs
@@ -4,8 +4,12 @@ namespace Whomever.Data.Entities
 {
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string SurName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? SurName { get; set; }
 
         //public int PhoneNumber { get; set; }
         //public string Address { get; set; }
