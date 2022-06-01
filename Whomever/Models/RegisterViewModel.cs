54358df9daf435e6a28ce217d32d67fbbfb72549
SusannahAndersson <susannah.alx@gmail.com>
2022-06-01 22:21:48 +0200

    added register user draft bf applicationuser db changes

---
+++ Whomever/Models/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿//using System.ComponentModel.DataAnnotations;
+
+//namespace Whomever.Models
+//{
+//    public class RegisterViewModel
+//    {
+//        [Required]
+//        public string UserName { get; set; }
+
+//        [Required]
+//        public string Password { get; set; }
+//    }
+//}
