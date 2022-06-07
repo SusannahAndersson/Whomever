05a6c0c0d918c9f326ad923b7392946dbaf7c3ed
SusannahAndersson <susannah.alx@gmail.com>
2022-06-07 15:06:47 +0200

    added checkout page

---
+++ Whomever/client/src/app/pages/checkoutPage.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Webshop } from "../services/webshop.service";
+
+
+@Component({
+  templateUrl: "checkoutPage.component.html",
+  styleUrls: ['checkoutPage.component.css']
+})
+export default class CheckoutPage implements OnInit {
+  constructor(public webshop: Webshop, private router: Router) {
+  }
+
+  ngOnInit() {
+    if (this.webshop.order.items.length === 0) {
+      this.router.navigate(["/"]);
+    }
+  }
+}
